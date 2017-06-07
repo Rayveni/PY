dc4490f20fa9ffc5f28d7af6e7d00e46069b5c8d
Rayveni <rjhbwf@gmail.com>
2017-06-07 23:49:30 +0300

    Add files via upload

---
+++ docx to markdow.py
@@ -0,0 +1,17 @@
+import mammoth
+import os
+path=r'C:\temp\Cтелазин.психология'
+save_path=r'C:\temp\Cтелазин.психология\save'
+beg='<a href=README.md><img src="../img/back.jpg" width="50" height="50" /></a><p><h1>%s</h1></p><p>'
+end='<a href=README.md><img src="../img/back.jpg" width="50" height="50" /></a>'
+
+def process_docx(filename):
+    with open(os.path.join(path,filename), "rb") as docx_file:
+        result=mammoth.convert_to_html(docx_file)
+        print(filename,result.messages)
+        f_name=filename[:-5]
+        with open(os.path.join(save_path,f_name+'.md'), "w", encoding='utf-8') as text_file:
+            text_file.write(beg % f_name+result.value+end)
+for file in os.listdir(path):
+    if file.endswith(".docx"):
+        process_docx(file)
