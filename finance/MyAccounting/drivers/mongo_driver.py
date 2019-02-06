336022168d12d60011c04d5e8aa3d642d9974976
Rayveni <rjhbwf@gmail.com>
2019-02-06 09:18:07 +0300

    Add files via upload

--- finance/MyAccounting/drivers/mongo_driver.py
+++ finance/MyAccounting/drivers/mongo_driver.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient,DESCENDING,ASCENDING
 from pandas import DataFrame
 
 class MongoDriver():
-    __slots__ ='db','accounts_info','accounts','documents','transactions','variables','__client','id_dict','mapping'
+    __slots__ ='db','accounts_info','accounts','documents','transactions','variables','__client','id_dict','mapping','config'
     def __init__(self,config: dict):
         
         self.__client=MongoClient(config['host'],config['port'])
@@ -14,13 +14,17 @@ class MongoDriver():
         self.transactions=self.db[config['transactions']]        
         self.variables=self.db[config['variables']]   
         
+        self.config={'currency': config['currency'],'exchange_rate': config['exchange_rate']}
         self.__add_indexes()
         self.__get_id_dict()
         
         self.mapping={'accounts_info':['account_id'],
                       'accounts':[None],
                       'documents':['document_id'],
-                      'transactions':['trans_id']}
+                      'transactions':['trans_id'],
+                      'currency':['currency_id'],
+                      'exchange_rate':['exchange_rate_id']
+                     }
         
     def __add_indexes(self) :
         if self.variables.find_one() is None:
@@ -29,7 +33,9 @@ class MongoDriver():
             self.documents.create_index([("document_id", ASCENDING), ("date", ASCENDING)],unique=True)
             self.transactions.create_index([("'trans_id", ASCENDING), ("trans_date", ASCENDING)],unique=True)
             self.variables.create_index([("var", DESCENDING)],unique=True)
-            
+            self.db[self.config['currency']].create_index([("code", DESCENDING)],unique=True)
+            self.db[self.config['exchange_rate']].create_index([("date", DESCENDING),("to_buy", DESCENDING),("to_sell", DESCENDING)],unique=True)			
+
     def __get_id_dict(self):
         res=self.variables.find_one({'var':'id_dict'})
         if res is None:
@@ -50,41 +56,62 @@ class MongoDriver():
         self.variables.update_one({'_id':self.id_dict[0]},{'$set':{'value.'+id_attr:next_attr_value}})   
         self.id_dict[1][id_attr]=next_attr_value
         return next_attr_value
+
+    def __max_id(self,collection, column)-> int:
+        res=collection.find_one({},sort=[(column,DESCENDING)])
+        if res is None:
+            res=-1
+        else:
+            res=res[column]
+        return res
+   
+    def __collection_pointer(self,table):
+        try:
+            collection=getattr(self,table)
+        except:
+            collection=self.db[self.config[table]]
+        return collection
     
-    def add_row(self,operation_type,obj,creation_date=None)->str:
+    def add_row(self,operation_type,obj,creation_date=None,rare_operation=False)->str:
         res={key:getattr(obj, key) for key in obj.__slots__}
         attr=self.mapping[operation_type][0]
         return_message=(True,'Ok')
-
-        if attr is not None:
-            res[attr]=self.__increment_id(attr)
-            
-        if creation_date is None:
-            res['creation_date']=datetime.datetime.now()
-        else:
-            res['creation_date']=creation_date
-        try:
-            getattr(self,operation_type).insert_one(res)
+        
+        collection=self.__collection_pointer(operation_type)
+       
+        try:           
+            if attr is not None:
+                if not rare_operation:
+                    res[attr]=self.__increment_id(attr)
+                else:
+                    res[attr]=self.__max_id(collection, attr)+1                 
+            if creation_date is None:
+                res['creation_date']=datetime.datetime.now()
+            else:
+                res['creation_date']=creation_date
+                print(res)
+            collection.insert_one(res)
         except Exception as e: 
             return_message=(False,str(e))
         return return_message
         
     def get_data(self,table,condition=None,dataframe=True):
-        collection=getattr(self,table)
+
+        collection=self.__collection_pointer(table)
         res=[x for x in collection.find({},{ "_id": False })]
         if dataframe:
             res=DataFrame(res)
         return res
 
     def get_element(self,table,column,value):
-        collection=getattr(self,table)
+        collection=self.__collection_pointer(table)
         res=collection.find_one({column:value})
-        return res	
-		
+        return res
+
     def update_element(self,table,search_column,search_value,dict_with_new_values):
-        collection=getattr(self,table)
+        collection=self.__collection_pointer(table)
         collection.update_one({search_column:search_value},{'$set':dict_with_new_values})  	
-		
+
     def delete_element(self,table,search_column,search_value):
-        collection=getattr(self,table)
-        collection.delete_one({search_column:search_value})  				
+        collection=self.__collection_pointer(table)
+        collection.delete_one({search_column:search_value})  
