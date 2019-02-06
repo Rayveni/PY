c245349a546f01ddbb5fb3b700eba704b4e52ee8
Rayveni <rjhbwf@gmail.com>
2019-02-06 09:25:58 +0300

    Delete currency - Copy.py

--- finance/MyAccounting/attributes/currency - Copy.py
+++
@@ -1,6 +0,0 @@
-class currency():
-    __slots__ ='code','symbol','description'
-    def __init__(self,code :str,symbol :str,description :str):
-        self.code=code
-        self.symbol=symbol		
-        self.description=description
